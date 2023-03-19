8c05be39552d8f1d388fb1429e43b02f0be87e54
Yudhriz <riskyyudha678@gmail.com>
2023-03-19 10:15:51 +0700

    Praktikum 04

---
+++ praktikum-04/template_admin/admin/index.php
@@ -0,0 +1,23 @@
+<?php
+    include_once 'top.php';
+    include_once 'menu.php';
+?>
+<div>
+    <div class="container-fluid px-4">
+        <!-- <h1>Selamat Datang di Halaman Admin</h1> -->
+    </div>
+    <?php 
+    error_reporting(0);
+    $hal = $_GET['hal'];
+    if($hal == 'dashboard.php'){
+        include_once 'dashboard.php';
+    } else if(!empty ($hal)) {
+        include_once ''.$hal.'.php';
+    } else {
+        include_once 'dashboard.php';
+    }
+    ?>
+</div>
+<?php
+    include_once 'bottom.php';
+?>
