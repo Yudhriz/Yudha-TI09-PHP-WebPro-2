5bc2b237f72956b0d61e38ef896a3b8db17ce33f
Yudhriz <riskyyudha678@gmail.com>
2023-03-19 10:12:18 +0700

    Praktikum 01

---
+++ praktikum-01/latihan-1.php
@@ -0,0 +1,12 @@
+<?php
+// Ini contoh deklarasi variable
+
+echo "Hello World";
+$nama = "Yudha";
+$umur = 20;
+
+// Ini contoh pemanggilan variable
+echo '<br> Nama = ' . $nama;
+echo '<br> Umur = ' . $umur;
+
+?>
---
+++ praktikum-01/latihan-2.php
@@ -0,0 +1,11 @@
+<?php
+
+define ('PHI',3.14);
+
+$jari = 8;
+$luas = PHI * $jari * $jari;
+$kll = 2 * PHI * $jari;
+
+echo 'Luas Lingkaran . '.$jari. ': '.$luas;
+
+?>
