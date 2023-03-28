9d1313bf9626b2778e24d913e64df90f572ff401
Yudhriz <riskyyudha678@gmail.com>
2023-03-28 09:20:39 +0700

    Praktikum 05

--- praktikum-05/pelanggan/form_pelanggan.php
+++ praktikum-05/pelanggan/form_pelanggan.php
@@ -97,17 +97,14 @@ require_once 'dbkoneksi.php';
          <?php
             }
         ?>
-        <!--
-        <option value="1">Elektronik</option>
-        <option value="2">Furniture</option>
-        <option value="3">Makanan</option>-->
 
       </select>
     </div>
   </div> 
     <div class="form-group row">
         <div class="offset-4 col-8">
-            <input type="submit" name="proses" type="submit" class="btn btn-primary" value="Simpan" />
+            <input type="submit" name="proses" class="btn btn-primary" value="Simpan" />
+            <input type="submit" name="proses" class="btn btn-primary" value="Update" />
         </div>
     </div>
 </form>
